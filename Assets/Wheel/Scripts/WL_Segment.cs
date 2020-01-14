aed0c082cfb79f908de3011ec356bf4aafd72d14
H.G <hugo.guerquin@outlook.fr>
2020-01-14 13:51:39 +0100

    Prêt pour faire l'éditeur

--- Assets/Wheel/Scripts/WL_Segment.cs
+++ Assets/Wheel/Scripts/WL_Segment.cs
@@ -9,15 +9,19 @@ public class WL_Segment : MonoBehaviour
 
     Color startColor = Color.black;
 
+    public bool SegmentActive { get; private set; } = false;
 
-    MeshCollider collider = null;
     MeshFilter filter = null;
     MeshRenderer rend = null;
+    WL_Wheel wheel = null;
 
+    public float StartAngle { get; private set; } = 0;
+    public float EndAngle { get; private set; } = 0;
 
-    int index = 0;
+    public int Index { get; private set; } = 0;
     float size = 0;
     int cuts = 0;
+    public bool MouseSelected { get; private set; } = false;
 
     private void OnMouseDown()
     {
@@ -26,66 +30,56 @@ public class WL_Segment : MonoBehaviour
 
     private void OnMouseEnter()
     {
-        rend.material.color = Color.red;
+        MouseSelected = true;
+        wheel.ClearSegments();
+        Activate(true);
     }
 
     private void OnMouseExit()
     {
+        MouseSelected = false;
         rend.material.color = startColor;
+        Activate(false);
     }
 
 
 
-    public void InitSight(int _index, float _size, int _cuts)
+    public void InitSight(int _index, float _size, int _cuts, WL_Wheel _wheel)
     {
+        wheel = _wheel;
         transform.localScale = Vector3.one;
         gameObject.layer = 5;
         gameObject.AddComponent<CanvasRenderer>();
-        collider = gameObject.AddComponent<MeshCollider>();
+        gameObject.AddComponent<MeshCollider>();
         filter = gameObject.AddComponent<MeshFilter>();
         rend = gameObject.AddComponent<MeshRenderer>();
         cuts = _cuts;
-        index = _index;
+        Index = _index;
         size = _size;
         DrawMesh();
         //DrawMeshOLD();
     }
 
-
-    void DrawMeshOLD()
+    public void Activate(bool _state)
     {
-        int _start = index * (360 / cuts);
-        int _end = _start + (360 / cuts);
-        int _angle = _end - _start;
-        int _vertices = _angle + 2;
-        Vector3[] _verticesPosition = new Vector3[_vertices];
-        int[] _triangles = new int[(_vertices - 2) * 3];
-        int _index = 0;
-        for (int i = _start; i < _end + 1; i++)
+        SegmentActive = _state;
+        if(_state)
         {
-            _verticesPosition[_index + 1] = transform.InverseTransformDirection((Quaternion.Euler(0, 0, -i) * Vector3.down).normalized * size);
-            if (_index < _vertices - 2)
-            {
-                _triangles[_index * 3] = 0;
-                _triangles[_index * 3 + 1] = _index + 1;
-                _triangles[_index * 3 + 2] = _index + 2;
-            }
-            _index++;
+            rend.material.color = Color.red;
         }
-        Mesh _mesh = new Mesh();
-        _mesh.vertices = _verticesPosition;
-        _mesh.triangles = _triangles;
-        _mesh.RecalculateNormals();
-        _mesh.RecalculateBounds();
-        filter.mesh = _mesh;
-        GetComponent<MeshCollider>().sharedMesh = _mesh;
-        startColor = rend.material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        else
+        {
+            rend.material.color = startColor;
+        }
     }
 
     void DrawMesh()
     {
-        float _start = index * (360f / cuts);
+        float _start = Index * (360f / cuts);
         float _end = _start + (360f / cuts);
+        StartAngle = _start;
+        EndAngle = _end;
+        name = $"{StartAngle}   {EndAngle}"; // DEBUG
         List<Vector3> _vertices = new List<Vector3>();
         List<int> _triangles = new List<int>();
 
@@ -114,12 +108,10 @@ public class WL_Segment : MonoBehaviour
         _triangles.Add(4);
         _triangles.Add(5);
 
-
         //
 
         // Segment
 
-        //List<Vector3> _arcPoints = new List<Vector3>();
         float _angle = _start;
         float _arcLength = _end - _start;
         for (int i = 0; i <= 360/cuts; i++)
@@ -136,31 +128,16 @@ public class WL_Segment : MonoBehaviour
         _triangles.Add(1);
         _triangles.Add(8);
 
-        //int _index = 0;
-
         for (int i = 7; i < _vertices.Count - 2; i++)
         {
             _triangles.Add(2);
             _triangles.Add(i + 1);
             _triangles.Add(i + 2);
-            //if (i % 3 == 0) _index++;
         }
-
-        //for (int i = 0; i < _pos.Length - 3; i+=3)
-        //{
-        //    _vertices[i + 6] = _midPos;
-        //    if (i == 0) _vertices[i + 7] = _vertices[1];
-        //    else _vertices[i + 7] = _vertices[i + 6 - 1];
-        //    _vertices[i + 8] = _pos[i + 2];
-        //    _triangles[i + 6] = i + 6;
-        //    _triangles[i + 7] = i + 7;
-        //    _triangles[i + 8] = i + 8;
-        //}
-
         //
 
         Mesh _mesh = new Mesh();
-        _mesh.name = $"Segment {index}  mesh";
+        _mesh.name = $"Segment {Index}  mesh";
         _mesh.vertices = _vertices.ToArray();
         _mesh.triangles = _triangles.ToArray();
         _mesh.RecalculateTangents();
@@ -169,17 +146,8 @@ public class WL_Segment : MonoBehaviour
         filter.mesh = _mesh;
         GetComponent<MeshCollider>().sharedMesh = _mesh;
 
-        startColor = rend.material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-        // debug
-        //rend.enabled = false;
+        rend.material = wheel.AllMats.Count > Index ? wheel.AllMats[Index] : rend.material;
+        startColor = rend.material.color;
     }
 
 }
-
-/*
- * calculer l'angle
- * faire un triangle simple du start à l'end
- * rajouter le segment manquant
- * done.
- */
- 
--- Assets/Wheel/Scripts/WL_Wheel.cs
+++ Assets/Wheel/Scripts/WL_Wheel.cs
@@ -2,63 +2,101 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.Events;
 
 public class WL_Wheel : MonoBehaviour
 {
-    [SerializeField, Range(1, 360)] int cuts = 4;
+    [SerializeField, Range(1, 180)] int cuts = 4;
     [SerializeField, Range(.1f, 10)] float size = 5;
 
-    List<GameObject> allSegments = new List<GameObject>();
+    [SerializeField] List<Material> allMats = new List<Material>();
+        public List<Material> AllMats { get { return allMats; } }
+    [SerializeField] List<UnityEvent> allEvents = new List<UnityEvent>();
+        public List<UnityEvent> AllEvents { get { return allEvents; } }
+
+    List<WL_Segment> allSegments = new List<WL_Segment>();
 
     int oldCuts = 0;
     float oldSize = 0;
+    float lastMousePos = 0;
 
     private void Start()
     {
         InitWheel();
         oldCuts = cuts;
         oldSize = size;
-        //StartCoroutine(CheckCutsModulo());
     }
 
     private void Update()
     {
+        if (Input.GetButtonDown("Wheel Submit")) ClickSegment();
         if (cuts != oldCuts || size != oldSize)
         {
-            //StartCoroutine(CheckCutsModulo());
             InitWheel();
         }
+        float _mousePos = Input.mousePosition.magnitude;
+        if (_mousePos == lastMousePos && !allSegments.Any(s => s.MouseSelected)) JoystickAngle();
+        lastMousePos = _mousePos;
         oldCuts = cuts;
         oldSize = size;
     }
 
-    IEnumerator CheckCutsModulo()
+    void ClickSegment()
     {
-        while (360 % cuts != 0)
-        {
-            if (cuts < 1)
-            {
-                cuts = 0;
-                yield break;
-            }
-            cuts--;
-            //yield return new WaitForSeconds(.01f);
-        }
+        WL_Segment _s = allSegments.Where(s => s.SegmentActive).FirstOrDefault();
+        if (!_s) return;
+        if(allEvents.Count > _s.Index) allEvents[_s.Index]?.Invoke();
     }
 
     void InitWheel()
     {
-        allSegments.ForEach(g => Destroy(g));
+        allSegments.ForEach(g => Destroy(g.gameObject));
         allSegments.Clear();
         for (int i = 0; i < cuts; i++)
         {
             GameObject _go = new GameObject($"Segment {i}", typeof(RectTransform));
-            allSegments.Add(_go);
             _go.transform.SetParent(transform);
             _go.transform.position = transform.position;
             WL_Segment _s = _go.AddComponent<WL_Segment>();
-            _s.InitSight(i, size, cuts);
+            allSegments.Add(_s);
+            _s.InitSight(i, size, cuts, this);
         }
     }
 
+    void JoystickAngle()
+    {
+        float _x = Input.GetAxis("Wheel Horizontal"), _y = Input.GetAxis("Wheel Vertical");
+        if(_x > -.3f && _x < .3f && _y > -.3f && _y < .3f)
+        {
+            ClearSegments();
+            return;
+        }
+        float _angle = Mathf.Asin(_y) * Mathf.Rad2Deg + 90;
+        if (_x < 0)
+        {
+            _angle += 180;
+            float _diff = 360 - _angle;
+            _angle = 180 + _diff;
+        }
+        if (_x < -.3f || _x > .3f || _y < -.3f || _y > .3f) SetSegmentActive(_angle);
+    }
+
+    public void ClearSegments()
+    {
+        foreach (WL_Segment _s in allSegments)
+        {
+            _s.Activate(false);
+        }
+    }
+
+    void SetSegmentActive(float _angle)
+    {
+        foreach(WL_Segment _s in allSegments)
+        {
+            _s.Activate(false);
+            if (_angle >= _s.StartAngle && _angle < _s.EndAngle) _s.Activate(true);
+        }
+        if (allSegments.Any(s => s.SegmentActive)) return;
+        else allSegments[0].Activate(true);
+    }
 }
